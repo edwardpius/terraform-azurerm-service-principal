6976f0ec38c9352729f94d1762ea48010fc0a955
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2018-08-03 22:37:37 +0200

    update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,16 @@
-provider "azurerm" {
-  # this should be bumped to 1.12.0 when it's released
-  version = "~> 1.11.0"
-}
-
-provider "random" {
-  version = "~> 1.3"
-}
-
 data "azurerm_subscription" "current" {}
 
-locals {
-  default_end_date = "${timeadd(timestamp(), "${var.years * 365}h")}"
-  end_date         = "${var.end_date != "" ? var.end_date : local.default_end_date}"
-  name             = "${var.name != "" ? var.name : random_id.default_name.hex}"
-  password         = "${var.password != "" ? var.password : random_string.default_password.result}"
-  scope            = "${var.scope != "" ? var.scope : data.azurerm_subscription.current.id}"
-}
-
-resource "random_string" "default_password" {
+resource "random_string" "password" {
   length = 32
 }
 
-resource "random_id" "default_name" {
+resource "random_id" "name" {
   byte_length = 16
   prefix      = "terraform"
 }
 
 resource "azurerm_azuread_application" "service_principal" {
-  name                       = "${local.name}"
-  available_to_other_tenants = false
+  name = "${coalesce(var.name, random_id.name.hex)}"
 }
 
 resource "azurerm_azuread_service_principal" "service_principal" {
@@ -37,12 +19,13 @@ resource "azurerm_azuread_service_principal" "service_principal" {
 
 resource "azurerm_azuread_service_principal_password" "service_principal" {
   service_principal_id = "${azurerm_azuread_service_principal.service_principal.id}"
-  value                = "${local.password}"
-  end_date             = "${local.end_date}"
+  value                = "${coalesce(var.password, random_string.password.result)}"
+  end_date             = "${coalesce(var.end_date, timeadd(timestamp(), "${var.years * (24 * 365)}h"))}"
 }
 
 resource "azurerm_role_assignment" "service_principal" {
-  scope                = "${local.scope}"
+  count                = "${var.skip_assignment ? 0 : 1}"
+  scope                = "${coalesce(var.scope, data.azurerm_subscription.current.id)}"
   role_definition_name = "${var.role}"
   principal_id         = "${azurerm_azuread_service_principal.service_principal.id}"
 }
